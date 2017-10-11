48cdfc54fa9c8c71eb848e4c4bf2940ab4ebe64e
HinsHuang <770834009@qq.com>
2017-10-11 09:57:01 +0800

    初步完成retrofit网络接口

---
+++ app/src/main/java/com/hins/reader/network/Api.java
@@ -0,0 +1,25 @@
+package com.hins.reader.network;
+
+import com.hins.reader.model.News;
+import com.hins.reader.model.StoryDetail;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Created by Hins on 2017/10/11.
+ */
+
+public interface Api {
+
+    @GET("api/4/news/latest")
+    Observable<News> getHomeStory();
+
+    @GET("api/4/news/before/{date}")
+    Observable<News> getBeforeHomeStory(@Path("date") String date);
+
+    @GET("api/4/news/{id}")
+    Observable<StoryDetail> getStoryDetail(@Path("id") int id);
+
+}
---
+++ app/src/main/java/com/hins/reader/network/HttpHelper.java
@@ -0,0 +1,74 @@
+package com.hins.reader.network;
+
+import com.hins.reader.config.Urls;
+import com.hins.reader.model.News;
+import com.hins.reader.model.StoryDetail;
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
+import io.reactivex.Observable;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Hins on 2017/10/11.
+ */
+
+public class HttpHelper {
+
+    private static HttpHelper INSTANCE;
+    private static Retrofit mRetrofit;
+
+    private HttpHelper() {
+        mRetrofit = new Retrofit.Builder()
+                .baseUrl(Urls.ZHIHU_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build();
+    }
+
+    public static HttpHelper getInstance() {
+        if (INSTANCE == null) {
+            synchronized (HttpHelper.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new HttpHelper();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    /**
+     * 获取最新的消息
+     * @return
+     */
+    public Observable<News> getLatestNews() {
+        return mRetrofit.create(Api.class)
+                .getHomeStory();
+    }
+
+
+    /**
+     * 获取以往的消息
+     * @param date 具体某一天
+     * @return
+     */
+    public Observable<News> getBeforeNews(String date) {
+        return mRetrofit.create(Api.class)
+                .getBeforeHomeStory(date);
+
+    }
+
+    /**
+     * 获取具体的消息内容
+     * @param id
+     * @return
+     */
+    public Observable<StoryDetail> getStoryDetail(int id) {
+        return mRetrofit.create(Api.class)
+                .getStoryDetail(id);
+    }
+
+
+
+
+}
